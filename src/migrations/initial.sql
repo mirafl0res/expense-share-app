bd2825685aa3a1fc9bc7c07471def72887dc9b29
mirafl0res <wasp.answer5v@icloud.com>
2026-02-27 15:48:57 +0100

    feat(db): create initial migration with users, groups, memberships, and expenses tables

---
+++ src/migrations/initial.sql
@@ -0,0 +1,32 @@
+-- Active: 1772199519469@@localhost@5432@expense-app-db@public
+
+CREATE TABLE users (
+    id UUID PRIMARY KEY,
+    username TEXT UNIQUE NOT NULL,
+    email TEXT UNIQUE NOT NULL,
+    password_hash TEXT NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT now(),
+    updated_at TIMESTAMP NOT NULL DEFAULT now()
+);
+
+CREATE TABLE expense_groups (
+    id UUID PRIMARY KEY,
+    title TEXT NOT NULL
+);
+
+CREATE TABLE expense_group_members (
+    user_id UUID REFERENCES users (id) ON DELETE CASCADE,
+    group_id UUID REFERENCES expense_groups (id) ON DELETE CASCADE,
+    PRIMARY KEY (user_id, group_id)
+);
+
+CREATE TABLE expenses (
+    id UUID PRIMARY KEY
+    group_id UUID REFERENCES groups(id) ON DELETE CASCADE,
+    payer_id UUID REFERENCES users(id) ON DELETE CASCADE,
+    title TEXT NOT NULL,
+    amount DECIMAL NOT NULL,
+    split_type TEXT NOT NULL,
+    expense_date TIMESTAMP NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT now(),
+    );
