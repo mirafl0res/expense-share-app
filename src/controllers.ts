8dddbbfb1a53e03058f7d7c7c15c7eb1416ada64
mirafl0res <wasp.answer5v@icloud.com>
2026-03-01 21:50:37 +0100

    feat(controllers): implement createExpense function

---
+++ src/controllers.ts
@@ -0,0 +1,15 @@
+import type { FastifyReply, FastifyRequest } from "fastify";
+import type { CreateExpenseRequest } from "./types";
+import { services } from "./services";
+
+export async function createExpense(
+  request: FastifyRequest<{ Body: CreateExpenseRequest; Params: {} }>,
+  reply: FastifyReply,
+): Promise<void> {
+  try {
+    const expense = await services.expenses.createExpense(request.body);
+    reply.status(201).send(expense);
+  } catch (error) {
+    reply.status(500).send({ error: "Internal server error" });
+  }
+}
