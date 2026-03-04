05ae7c870d718c397a63fcd9b273fc086362faad
mirafl0res <wasp.answer5v@icloud.com>
2026-03-04 16:23:46 +0100

    refactor(errors): update error constructors to use options object for better flexibility

--- src/errors.ts
+++ src/errors.ts
@@ -1,10 +1,19 @@
+type ErrorOptions = {
+  message?: string;
+  params?: Record<string, unknown>;
+  cause?: unknown;
+};
+
 export abstract class BaseError extends Error {
   abstract statusCode: number;
   params: Record<string, unknown> = {};
 
-  constructor(message: string, cause?: unknown) {
+  constructor(options: ErrorOptions = {}) {
+    const { message, params = {}, cause } = options;
+
     super(message, { cause });
     this.name = new.target.name;
+    this.params = params;
   }
 
   toPublicError() {
@@ -20,89 +29,82 @@ export abstract class BaseError extends Error {
 export class InternalError extends BaseError {
   statusCode = 500;
 
-  constructor(
-    message: string = "Internal server error",
-    params: Record<string, unknown> = {},
-    cause?: unknown,
-  ) {
-    super(message, cause);
-    this.params = params;
+  constructor(options: ErrorOptions = {}) {
+    super({
+      message: options.message ?? "Internal server error",
+      params: options.params,
+      cause: options.cause,
+    });
   }
 }
 
 export class NotFoundError extends BaseError {
   statusCode = 404;
-
-  constructor(
-    message: string = "Resource not found",
-    params: Record<string, unknown> = {},
-    cause?: unknown,
-  ) {
-    super(message, cause);
-    this.params = params;
+  constructor(options: ErrorOptions = {}) {
+    super({
+      message: options.message ?? "Resource not found",
+      params: options.params,
+      cause: options.cause,
+    });
   }
 }
 
 export class BadRequestError extends BaseError {
   statusCode = 400;
 
-  constructor(
-    message: string = "Bad request",
-    params: Record<string, unknown> = {},
-    cause?: unknown,
-  ) {
-    super(message, cause);
-    this.params = params;
+  constructor(options: ErrorOptions = {}) {
+    super({
+      message: options.message ?? "Bad request",
+      params: options.params,
+      cause: options.cause,
+    });
   }
 }
 
 export class ConflictError extends BaseError {
   statusCode = 409;
-  constructor(
-    message: string = "Conflict",
-    params: Record<string, unknown> = {},
-    cause?: unknown,
-  ) {
-    super(message, cause);
-    this.params = params;
+
+  constructor(options: ErrorOptions = {}) {
+    super({
+      message: options.message ?? "Conflict error",
+      params: options.params,
+      cause: options.cause,
+    });
   }
 }
 
 export class AuthenticationError extends BaseError {
   statusCode = 401;
 
-  constructor(
-    message: string = "Not authenticated",
-    params: Record<string, unknown> = {},
-    cause?: unknown,
-  ) {
-    super(message, cause);
-    this.params = params;
+  constructor(options: ErrorOptions = {}) {
+    super({
+      message: options.message ?? "Authentication error",
+      params: options.params,
+      cause: options.cause,
+    });
   }
 }
 
 export class ForbiddenError extends BaseError {
   statusCode = 403;
 
-  constructor(
-    message: string = "Access forbidden",
-    params: Record<string, unknown> = {},
-    cause?: unknown,
-  ) {
-    super(message, cause);
-    this.params = params;
+  constructor(options: ErrorOptions = {}) {
+    super({
+      message: options.message ?? "Access forbidden",
+      params: options.params,
+      cause: options.cause,
+    });
   }
 }
 
 export class DatabaseError extends BaseError {
   statusCode = 500; // Internal server error
 
-  constructor(
-    message: string = "Database error",
-    params: Record<string, unknown> = {},
-    cause?: unknown,
-  ) {
-    super(message, cause);
-    this.params = params;
+  constructor(options: ErrorOptions = {}) {
+    super({
+      message: options.message ?? "Database error",
+      params: options.params,
+      cause: options.cause,
+    });
   }
 }
