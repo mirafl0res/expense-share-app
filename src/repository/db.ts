01d11d289397291fe2969e835ce7f11deece5014
mirafl0res <wasp.answer5v@icloud.com>
2026-03-07 15:19:26 +0100

    feat(repository): implement global SQL instance to prevent 'PostgresError: too many clients' on hot reloads

--- src/repository/db.ts
+++ src/repository/db.ts
@@ -1,5 +1,15 @@
 import { SQL } from "bun";
 
-const db = new SQL({});
+// const db = new SQL({});
+// export default db;
+
+
+const globalKey = Symbol.for("myapp.db");
+
+if (!(globalThis as any)[globalKey]) {
+  (globalThis as any)[globalKey] = new SQL({});
+}
+
+const db = (globalThis as any)[globalKey];
 
 export default db;
