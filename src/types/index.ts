272a01b489d087d5384b7a1f0be72e17bb27f957
mirafl0res <wasp.answer5v@icloud.com>
2026-03-16 13:41:30 +0100

    refactor(auth): reorganize type exports for simpler imports

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,8 @@
 export type { User, UserCreateRequest } from "./users";
 export type { Group, GroupCreateRequest } from "./groups";
 export type { Expense, ExpenseCreateRequest } from "./expenses";
+export type {
+  Auth0TokenRequestParams,
+  Auth0TokenResponse,
+  AuthCodeExchangeRequest,
+} from "./auth0";
