88c22df860c10a85a40119490afed1e2bfdaec34
mirafl0res <wasp.answer5v@icloud.com>
2026-03-18 10:17:06 +0100

    refactor(auth): comment out unused code

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,13 +1,13 @@
-import type { FastifyInstance, FastifyPluginOptions } from "fastify";
-import * as authController from "../controllers/auth";
+// import type { FastifyInstance, FastifyPluginOptions } from "fastify";
+// import * as authController from "../controllers/auth0";
 
-export async function authRoutes(
-  fastify: FastifyInstance,
-  _opts: FastifyPluginOptions,
-) {
-  fastify.route({
-    method: "GET",
-    url: "/auth/callback",
-    handler: authController.authCallbackHandler,
-  });
-}
+// export async function authRoutes(
+//   fastify: FastifyInstance,
+//   _opts: FastifyPluginOptions,
+// ) {
+//   fastify.route({
+//     method: "GET",
+//     url: "/auth/callback",
+//     handler: authController.authCallbackHandler,
+//   });
+// }
